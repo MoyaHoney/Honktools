3f27e2eaafa427ea63dadb5a7ccc5645e9abca9f
mrhonk123 <justsweers@comcast.net>
2018-11-25 13:17:56 -0800

    working on func Skeleton_Folders

---
+++ tools.py
@@ -0,0 +1,49 @@
+import os
+
+
+def skeleton_folders(*args):
+    """Makes a copy of a folder and all subfolders, while ignoring files."""
+    # Testing if *args have been filled out.
+    if len(args) == 0:
+        args_filled = False
+        mode_args = False
+    elif len(args) > 0:
+        args_filled = True
+        number_of_filled_args = len(args)
+
+    # Output statement if *args filled out incorrectly
+    def arg_error():
+        print("""You have supplied """ + str(len(args)) + """ arguments. Either you have not supplied valid
+        arguments, or too few arguments. Function will now continue in a 'input mode'""")
+        # TODO Replace with a proper logging/raise.
+
+    # Checks to see if a the supplied output directory should be created if it does not exist.
+    def ask_for_arg3():
+        print("It looks like ", end="")
+        if args_filled:
+            print(args[1], end="")
+        else:
+            pass  # TODO fix else statement to print the dir if *args were not supplied. Don't forget (, end="").
+        print(" does not exist.")
+
+        create_2nd_dir = None
+        while create_2nd_dir != "Y" or create_2nd_dir != "N":
+            create_2nd_dir = (input("Would you like to create it?\nY/N")).upper()
+
+    # Testing if *args has been filled correctly.
+    # If *args only has a single argument, raise an error
+    if args_filled:
+        if number_of_filled_args == 1:
+            arg_error()
+        # If *args has two statements, check if both dirs are correct.
+        # Checking first...
+        elif number_of_filled_args == 2:
+            if os.path.isdir(args[0]):
+                pass
+            else:
+                arg_error()
+            # Checking second...
+            if os.path.isdir(args[1]):
+                pass
+            else:
+                ask_for_arg3()
